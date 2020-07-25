2153c679863fa0cabeb1bdbe5290beb8826ad0eb
Ollrogge <nils-ollrogge@outlook.de>
2020-07-25 13:05:57 +0200

    ctap_hid doxygen defs

--- ctaphid/include/ctap_hid.h
+++ ctaphid/include/ctap_hid.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2020 Nils Ollrogge
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    CTAP2 implementation
+ * @ingroup     FIDO2
+ * @brief       CTAP2 implementation
+ *
+ * @{
+ *
+ * @file
+ * @brief       CTAP2 interface
+ *
+ * @author      Nils Ollrogge <nils-ollrogge@outlook.de>
+ */
+
 #ifndef CTAP_HID_H
 #define CTAP_HID_H
 
@@ -5,57 +26,121 @@
 #include "usb/usbus/hid.h"
 #include "timex.h"
 
-#define CTAP_HID_INIT_PAYLOAD_SIZE  (CONFIG_USBUS_HID_INTERRUPT_EP_SIZE-7)   /* endpoint size - init packet metadata */
-#define CTAP_HID_CONT_PAYLOAD_SIZE  (CONFIG_USBUS_HID_INTERRUPT_EP_SIZE-5)   /* endpoint size - cont packet metadata */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+/**
+ * @name CTAP_HID packet type payload sizes
+ * 
+ * @{
+ */
+#define CTAP_HID_INIT_PAYLOAD_SIZE  (CONFIG_USBUS_HID_INTERRUPT_EP_SIZE-7)   /**< endpoint size - init packet metadata */
+#define CTAP_HID_CONT_PAYLOAD_SIZE  (CONFIG_USBUS_HID_INTERRUPT_EP_SIZE-5)   /**< endpoint size - cont packet metadata */
+/** @} */
+
+/**
+ * @brief CTAP_HID protocol version
+ */
 #define CTAP_HID_PROTOCOL_VERSION 0x02
 
+/**
+ * @name CTAP_HID packet type identifiers
+ * 
+ * @{
+ */
 #define CTAP_HID_INIT_PACKET 0x80
 #define CTAP_HID_CONT_PACKET 0x00
+/** @} */
 
 /* todo: both timeouts are arbitrary */
-#define CTAP_HID_TRANSACTION_TIMEOUT (1 * US_PER_SEC) /* cont packet not sent in time */
-#define CTAP_HID_INACTIVITY_TIMEOUT (1 * US_PER_SEC)
+#define CTAP_HID_TRANSACTION_TIMEOUT    (1 * US_PER_SEC) /* cont packet not sent in time */
+#define CTAP_HID_INACTIVITY_TIMEOUT     (1 * US_PER_SEC)
 
+/**
+ * @brief CTAP_HID max message payload size
+ * 
+ * CTAP specification (version 20190130) section 8.2.4.
+ */
+#define CTAP_HID_BUFFER_SIZE 7609
 
-/* CTAP specification (version 20190130) section 8.1.4. */
-#define CTAP_HID_BUFFER_SIZE 7609 /* 64 - 7 + 128 * (64 - 5) */
+/**
+ * @name CTAP_HID commands
+ * 
+ * @{
+ */
+#define CTAP_HID_COMMAND_PING       (0x01 | CTAP_HID_INIT_PACKET)
+#define CTAP_HID_COMMAND_MSG        (0x03 | CTAP_HID_INIT_PACKET)
+#define CTAP_HID_COMMAND_LOCK       (0x04 | CTAP_HID_INIT_PACKET)
+#define CTAP_HID_COMMAND_INIT       (0x06 | CTAP_HID_INIT_PACKET)
+#define CTAP_HID_COMMAND_WINK       (0x08 | CTAP_HID_INIT_PACKET)
+#define CTAP_HID_COMMAND_CBOR       (0x10 | CTAP_HID_INIT_PACKET)
+#define CTAP_HID_COMMAND_CANCEL     (0x11 | CTAP_HID_INIT_PACKET)
+#define CTAP_HID_COMMAND_KEEPALIVE  (0x3b | CTAP_HID_INIT_PACKET)
+#define CTAP_HID_COMMAND_ERROR      (0x3f | CTAP_HID_INIT_PACKET)
+/** @} */
 
-#define CTAP_HID_COMMAND_PING (0x01 | CTAP_HID_INIT_PACKET)
-#define CTAP_HID_COMMAND_MSG (0x03 | CTAP_HID_INIT_PACKET)
-#define CTAP_HID_COMMAND_LOCK (0x04 | CTAP_HID_INIT_PACKET)
-#define CTAP_HID_COMMAND_INIT (0x06 | CTAP_HID_INIT_PACKET)
-#define CTAP_HID_COMMAND_WINK (0x08 | CTAP_HID_INIT_PACKET)
-#define CTAP_HID_COMMAND_CBOR (0x10 | CTAP_HID_INIT_PACKET)
-#define CTAP_HID_COMMAND_CANCEL (0x11 | CTAP_HID_INIT_PACKET)
-#define CTAP_HID_COMMAND_KEEPALIVE (0x3b | CTAP_HID_INIT_PACKET)
-#define CTAP_HID_COMMAND_ERROR (0x3f | CTAP_HID_INIT_PACKET)
+/**
+ * @name CTAP_HID capability flags
+ * 
+ * @{
+ */
+#define CTAP_HID_CAPABILITY_WINK 0x01   /**< If set, authenticator implements CTAPHID_WINK function */
+#define CTAP_HID_CAPABILITY_CBOR 0x04   /**< If set, authenticator implements CTAPHID_CBOR function */
+#define CTAP_HID_CAPABILITY_NMSG 0x08   /**< If set, authenticator DOES NOT implement CTAPHID_MSG function (CTAP1 / U2F) */
+/** @} */
 
-#define CTAP_HID_CAPABILITY_WINK 0x01       /* If set to 1, authenticator implements CTAPHID_WINK function */
-#define CTAP_HID_CAPABILITY_CBOR 0x04       /* If set to 1, authenticator implements CTAPHID_CBOR function */
-#define CTAP_HID_CAPABILITY_NMSG 0x08       /* If set to 1, authenticator DOES NOT implement CTAPHID_MSG function (CTAP1 / U2F) */
+/**
+ * @name CTAP_HID error codes
+ * 
+ * @{
+ */
+#define CTAP_HID_ERROR_INVALID_CMD      0x01    /**< The command in the request is invalid */
+#define CTAP_HID_ERROR_INVALID_PAR      0x02    /**< The parameter(s) in the request is invalid */
+#define CTAP_HID_ERROR_INVALID_LEN      0x03    /**< The length field (BCNT) is invalid for the request */
+#define CTAP_HID_ERROR_INVALID_SEQ      0x04    /**< The sequence does not match expected value */
+#define CTAP_HID_ERROR_MSG_TIMEOUT      0x05    /**< The message has timed out */
+#define CTAP_HID_ERROR_CHANNEL_BUSY     0x06    /**< The device is busy for the requesting channel */
+#define CTAP_HID_ERROR_LOCK_REQUIRED    0x0a    /**< Command requires channel lock */
+#define CTAP_HID_ERROR_INVALID_CHANNEL  0x0b    /**< CID is not valid. */
+#define CTAP_HID_ERROR_OTHER            0x7f    /**< Unspecified error */
+/** @} */
 
-#define CTAP_HID_BROADCAST_CID 0xffffffff
-
-#define CTAP_HID_ERROR_INVALID_CMD 0x01         /* The command in the request is invalid */
-#define CTAP_HID_ERROR_INVALID_PAR 0x02         /* The parameter(s) in the request is invalid */
-#define CTAP_HID_ERROR_INVALID_LEN 0x03         /* The length field (BCNT) is invalid for the request */
-#define CTAP_HID_ERROR_INVALID_SEQ 0x04         /* The sequence does not match expected value */
-#define CTAP_HID_ERROR_MSG_TIMEOUT 0x05         /* The message has timed out */
-#define CTAP_HID_ERROR_CHANNEL_BUSY 0x06        /* The device is busy for the requesting channel */
-#define CTAP_HID_ERROR_LOCK_REQUIRED 0x0a       /* Command requires channel lock */
-#define CTAP_HID_ERROR_INVALID_CHANNEL 0x0b     /* CID is not valid. */
-#define CTAP_HID_ERROR_OTHER 0x7f               /* Unspecified error */
-
-#define CTAP_HID_STATUS_PROCESSING 0x01
+/**
+ * @name CTAP_HID status codes
+ * 
+ * @{
+ */
+#define CTAP_HID_STATUS_PROCESSING 0x01 
 #define CTAP_HID_STATUS_UPNEEDED   0x02
+/** @} */
 
+/**
+ * @brief CTAP_HID max number of channels
+ * 
+ */
 #define CTAP_HID_CIDS_MAX 0x08
 
-void ctap_hid_create(void);
-void ctap_hid_handle_packet(uint8_t *pkt_raw);
-void ctap_hid_send_keepalive(uint8_t status);
+/**
+ * @brief CTAP_HID broadcast channel identifier
+ * 
+ */
+#define CTAP_HID_BROADCAST_CID 0xffffffff
 
+/**
+ * @name CTAP_HID buffer status
+ * 
+ * @{
+ */
+#define CTAP_HID_BUFFER_STATUS_BUFFERING    0x00
+#define CTAP_HID_BUFFER_STATUS_DONE         0x01
+#define CTAP_HID_BUFFER_STATUS_ERROR        0x02
+/** @} */
+
+/**
+ * @brief CTAP_HID initialization packet struct
+ * 
+ */
 typedef struct
 {
     uint8_t cmd;
@@ -64,12 +149,20 @@ typedef struct
     uint8_t payload[CTAP_HID_INIT_PAYLOAD_SIZE];
 } ctap_hid_init_pkt_t;
 
+/**
+ * @brief CTAP_HID continuation packet struct
+ * 
+ */
 typedef struct
 {
     uint8_t seq;
     uint8_t payload[CTAP_HID_CONT_PAYLOAD_SIZE];
 } ctap_hid_cont_pkt_t;
 
+/**
+ * @brief CTAP_HID packet struct
+ * 
+ */
 typedef struct
 {
     uint32_t cid;
@@ -79,6 +172,10 @@ typedef struct
     };
 } ctap_hid_pkt_t;
 
+/**
+ * @brief CTAP_HID initialization response struct
+ * 
+ */
 typedef struct __attribute__((packed))
 {
     uint8_t nonce[8];
@@ -90,6 +187,10 @@ typedef struct __attribute__((packed))
     uint8_t capabilities;
 } ctap_hid_init_resp_t;
 
+/**
+ * @brief CTAP_HID channel identifier struct
+ * 
+ */
 typedef struct
 {
     uint8_t taken;
@@ -97,11 +198,10 @@ typedef struct
     uint64_t last_used;
 } ctap_hid_cid_t;
 
-
-#define CTAP_HID_BUFFER_STATUS_BUFFERING 0x00
-#define CTAP_HID_BUFFER_STATUS_DONE 0x01
-#define CTAP_HID_BUFFER_STATUS_ERROR 0x02
-
+/**
+ * @brief CTAP_HID buffer struct
+ * 
+ */
 typedef struct
 {
     uint32_t cid;
@@ -114,4 +214,29 @@ typedef struct
     uint8_t locked;
 } ctap_hid_buffer_t;
 
+/**
+ * @brief Initialize USB, create needed threads
+ *
+ */
+void ctap_hid_create(void);
+
+/**
+ * @brief Handle CTAP_HID packet
+ *
+ * @param[in] pkt_raw   CTAP_HID packet
+ * 
+ */
+void ctap_hid_handle_packet(uint8_t *pkt_raw);
+
+/**
+ * @brief Send keepalive packet
+ *
+ * @param[in] status    CTAP_HID status code
+ * 
+ */
+void ctap_hid_send_keepalive(uint8_t status);
+
+#ifdef __cplusplus
+}
+#endif
 #endif
